a0c5a2d39d0642c317ce46b2bd3f690c9e9fff7e
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-03-10 16:58:12 +0100

    feat: J1/Job 4

---
+++ jour01/job04/script.js
@@ -0,0 +1,9 @@
+console.log("Hello JavaScript !");
+
+function bissextile(année) {
+    if (année % 4 == 0) {
+        console.log(année + ' est bissextile')
+    } else {
+        console.log(année + ' n\'est pas bissextile')
+    }
+}
