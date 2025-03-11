1c66765bbf4f59d43f0acd9ed7fa618c64dacba1
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-03-11 09:28:34 +0100

    feat: J1/Job 5

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+console.log("Hello JavaScript !");
+
+function afficherJoursSemaine() {
+    let JoursSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let jour of JoursSemaine) {
+        console.log(jour);
+    }
+}
+
+afficherJoursSemaine();
