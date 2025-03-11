0faade7e7b5c95aed173eb22d9e9bc6ecffc7ba7
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-03-11 15:44:19 +0100

    feat: J2/Job 1

---
+++ jour02/job01/script.js
@@ -0,0 +1,5 @@
+function citation () {
+            const msg = document.getElementById("citation").textContent;
+            console.log(msg);
+        }
+        document.getElementById("button").addEventListener("click", citation)
