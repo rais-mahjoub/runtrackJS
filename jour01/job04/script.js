9407658556c76e58ee2ba6c8260506805781777c
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-03-11 09:22:09 +0100

    fix: J1/Job 4

--- jour01/job04/script.js
+++ jour01/job04/script.js
@@ -1,7 +1,7 @@
 console.log("Hello JavaScript !");
 
 function bissextile(année) {
-    if (année % 4 == 0) {
+    if (année % 4 == 0 && année % 100 != 0 || année % 400 == 0) {
         console.log(année + ' est bissextile')
     } else {
         console.log(année + ' n\'est pas bissextile')
